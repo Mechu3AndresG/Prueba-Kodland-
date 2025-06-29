a97c6c740195149e4931f7b0ccb69c33d348cdf0
Andres Gaitan <andrescamilo.natacion@gmail.com>
2025-06-28 19:16:12 -0500

    actualizacion de movimiento, rotacion y disparos

--- main.py
+++ main.py
@@ -1,33 +1,113 @@
 import pygame,sys,constantes
 from personaje import Personaje
+#from bullet import Bullet
 
 pygame.init();
 
-jugador=Personaje(50,50)
-
 #ventana
-screen = pygame.display.set_mode((constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA));
-
+ventana = pygame.display.set_mode((constantes.ANCHO_VENTANA,constantes.ALTO_VENTANA));
 #titulo e icono 
 pygame.display.set_caption("Juego Kodland")
 icono=pygame.image.load("assets/Soldado/Soldier-Guy-PNG/Arma de Fuego/01-Idle/E_E_Gun__Idle_001.png")
 pygame.display.set_icon(icono)
 
+def escalarImg(image, scala):
+    w = image.get_width()
+    h = image.get_height()
+    nueva_imagen = pygame.transform.scale(image,(w*scala,
+                                                    h*scala))
+    return nueva_imagen 
+
+#aparicion primera del jugador
+imagenJugador = pygame.image.load("assets/Soldado/Soldier-Guy-PNG/Arma de Fuego/01-Idle/E_E_Gun__Idle_002.png")
+imagenJugador=escalarImg(imagenJugador, constantes.SCALA_PERSONAJE)
+
+#imagen bala
+
+
+animacionesCaminar=[]
+animacionesDisparo=[]
+animacionesMuerte=[]
+animacionesCaminar.append(imagenJugador)
+for i in range (9):
+    img = pygame.image.load(f"assets/Soldado/Soldier-Guy-PNG/Arma de Fuego/02-Run/E_E_Gun__Run_000_00{i}.png ")
+    img = escalarImg(img, constantes.SCALA_PERSONAJE)
+    
+    imgShot = pygame.image.load(f"assets/Soldado/Soldier-Guy-PNG/Arma de Fuego/03-Shot/E_E_Gun__Attack_00{i}.png")
+    imgShot = escalarImg(imgShot, constantes.SCALA_PERSONAJE)
+    
+    animacionesCaminar.append(img)
+    animacionesDisparo.append(imgShot)
+
+jugador=Personaje(508,450, animacionesCaminar, animacionesDisparo)
+
+
+
 #menu
 
 
 #juego
-fondo= pygame.image.load("assets/postapocalypse_bg_pixel_png/PNG/Postapocalypce4/Bright/postapocalypse4.png").convert()
-screen.blit(fondo,(0,0))
+fondo = pygame.image.load("assets/postapocalypse_bg_pixel_png/PNG/Postapocalypce4/Bright/postapocalypse4.png").convert()
+ventana.blit(fondo,(0,0))
 
-#personaje
-#caminaDerecha = [pygame.image.load()] 
+#personaje control movimiento
+caminaDerecha = False
+caminarIzquierda = False
+
+#disparar
+dispara=False
+
+#controlar frame rate
+reloj = pygame.time.Clock()
 while True:
+    #60 fps
+    reloj.tick(60)
+    #actualizar fondo
+    ventana.blit(fondo,(0,0))
+    #inicializar pasos
+    deltaX = 0
+    
+    #actualiza delta
+    if caminaDerecha==True:
+        deltaX=constantes.VELOCIDAD
+    if caminarIzquierda==True:
+        deltaX=-constantes.VELOCIDAD
+
+     #mover Jugador
+    jugador.movimiento(deltaX) 
+
+    jugador.update()
+
+    jugador.dibujar(ventana)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit();
             sys.exit()
+        #Calcular movimiento
+        if event.type== pygame.KEYDOWN:
+            #tecla abajo
+            if event.key == pygame.K_a:
+                caminarIzquierda=True
+            if event.key == pygame.K_d:
+                caminaDerecha=True
+            #tecla disparo
+            if event.key == pygame.K_f:
+                dispara = True
+                jugador.shot(dispara)
+
+        if event.type== pygame.KEYUP:
+            #tecla arriba
+            if event.key == pygame.K_a:
+                caminarIzquierda=False
+            if event.key == pygame.K_d:
+                caminaDerecha=False
+            
+        
+           
+            
     pygame.display.update()
+    
+
 
 
     
--- personaje.py
+++ personaje.py
@@ -1,9 +1,50 @@
-import pygame
+import pygame, constantes
 
 class Personaje():
-    def __init__(self, x, y):
-        self.shape = pygame.Rect(0,0,20,20)
-        self.shape.center = (x,y)
+    def __init__(self, x, y, animacionesCaminar, animacionesDisparo):
+        self.flip = False
+        self.disparo=False
+        self.animaciones=animacionesCaminar
+        self.animacionesDisparo = animacionesDisparo
+        #imagen de la animacion de que esta mostrando
+        self.frame_index = 0
+        self.image = animacionesCaminar[self.frame_index]
+        #actualiza el tiempo desde que se inicializo pygame
+        self.update_time = pygame.time.get_ticks()
+        self.forma = self.image.get_rect()
+        self.forma.center = (x,y)
+    
+    def update(self):
+        cooldown_animacion = 100
+        if self.disparo:
+            self.image = self.animacionesDisparo[self.frame_index]   
+        else:
+            self.image = self.animaciones[self.frame_index]
+        if pygame.time.get_ticks() - self.update_time >= cooldown_animacion:
+            self.frame_index = self.frame_index + 1
+            self.update_time = pygame.time.get_ticks()
+        if self.disparo and self.frame_index >= len(self.animacionesDisparo):
+            self.disparo = False
+            self.frame_index = 0
+        elif not self.disparo and self.frame_index >= len(self.animaciones):
+            self.frame_index = 0
+    
+    def shot(self, shoting):
+        if shoting:
+            self.disparo=True
+            self.frame_index=0
+            self.update_time=pygame.time.get_ticks()
+                
+
 
     def dibujar(self, interfaz):
-        pygame.draw.rect(interfaz,(255,255,0),self.shape)
+        imagen_flip= pygame.transform.flip(self.image, self.flip, False)
+        interfaz.blit(imagen_flip, self.forma)
+        pygame.draw.rect(interfaz,constantes.COLOR_PERSONAJE,self.forma,1)
+
+    def movimiento (self, deltaX):
+        if deltaX < 0:
+            self.flip=True
+        if deltaX > 0:
+            self.flip=False
+        self.forma.x=self.forma.x + deltaX
